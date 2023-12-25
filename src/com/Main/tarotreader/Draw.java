2236b61e5efdc4d940b19000e1d16fc113e1b0de
Lya <lasvmateo@gmail.com>
2023-12-26 00:19:44 +0100

    Refactor createCardList method for improved clarity and add debug statement to NewLuckRatio method

--- src/com/Main/tarotreader/Draw.java
+++ src/com/Main/tarotreader/Draw.java
@@ -49,10 +49,11 @@ public class Draw {
     
     /**
      * Creates a list of integers representing the cards in the deck.
-     *
+     *might rename this method to createDeck for improved clarity
+
      * @return A list of integers representing the cards in the deck.
      */
-    public List<Integer> createCardList() {
+    public List<Integer> createCardList() {//works perfectly
         List<Integer> cardList = new ArrayList<>();
         for (int i = 1; i < this.CardsInDeck+1; i++) {
             cardList.add(i);
@@ -66,7 +67,7 @@ public class Draw {
      * Prompts the user to input the number of additional cards they would like to draw,
      * between 0 and 3, and updates the total number of cards to be drawn accordingly.
      */
-    public void askAdditionalDraws() {
+    public void askAdditionalDraws() {//works perfectly
         Scanner scanner = new Scanner(System.in);
         System.out.print("You can add between 0 and 3 cards to your initial draw of 3.\nHow many additional cards would you like to draw if any?\n");
         int additionalDraws = scanner.nextInt();
@@ -83,15 +84,15 @@ public class Draw {
      * @param drawnCards The array of drawn cards.
      * @return The new Luck ratio array.
      */
-    public int[] NewLuckRatio(int[] drawnCards) {
-        int[] LuckRatio = new int[drawnCards.length * 2];
-        int index = 0;
+    public List<Pair<Integer, Integer>> NewLuckRatio(int[] drawnCards) {
+        List<Pair<Integer, Integer>> LuckRatio = new ArrayList<>();
         
         for (int i = 0; i < drawnCards.length; i++) {
             int randomNum = (int) (Math.random() * 21) - 10; // Generate a random number between -10 and 10
-            LuckRatio[index++] = drawnCards[i];
-            LuckRatio[index++] = randomNum;
+            LuckRatio.add(new Pair<>(drawnCards[i], randomNum));
         }
+        
+        System.out.println("The Luck Ratio is: " + LuckRatio); // debug
         
         return LuckRatio;
     }
@@ -102,7 +103,7 @@ public class Draw {
      * @return An array of integers representing the drawn cards.
      */
     public int[] NewDraw() {
-2        this.askAdditionalDraws();
+       this.askAdditionalDraws();
         this.DrawnCards = new int[this.TotalDraw];
         List<Integer> cardList = createCardList(); // Get the initial card list
 
